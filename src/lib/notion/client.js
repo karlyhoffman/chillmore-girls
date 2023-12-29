7ab7363df52452cc241ee301dacfa1caf4743e56
Karly Hoffman <karlyhoffman@gmail.com>
2023-12-29 13:50:13 -0600

    minor tweaks

--- src/lib/notion/client.js
+++ src/lib/notion/client.js
@@ -4,9 +4,10 @@ const auth = import.meta.env.NOTION_TOKEN;
 
 export const notion = new Client({ auth });
 
-export async function getSome() {
+export async function getSome({ options = {} }) {
   const response = await notion.databases.query({
     database_id: import.meta.env.NOTION_DATABASE_ID,
+    ...options,
   });
 
   return response;
